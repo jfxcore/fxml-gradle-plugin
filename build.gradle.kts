2ba9262a382293576277978802f5d03c634da854
mstr2 <43553916+mstr2@users.noreply.github.com>
2026-09-26 01:41:27 +0200

    Bump version to 0.18.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 
 dependencies {
     implementation(gradleApi())
-    implementation("org.jfxcore:fxml-compiler:0.17.1")
+    implementation("org.jfxcore:fxml-compiler:0.18.0")
 
     testImplementation(gradleTestKit())
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
